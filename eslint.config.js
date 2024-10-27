a92141ba6ddb8fa3dd7ba2ee015e06c914fcd841
Kennedy Maombi <kennedy@itskennedy.dev>
2024-10-27 02:08:38 -0500

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,56 @@
-// .eslintrc.js
-module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      project: './tsconfig.json',
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true
-      }
+const typescript = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+const react = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const nextPlugin = require('@next/eslint-plugin-next');
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'dist/**'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,  
+        ecmaVersion: 2024,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
     },
-    plugins: ['@typescript-eslint', 'react'],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended'
-    ],
+    settings: {
+      react: {
+        version: 'detect'  
+      },
+      next: {
+        rootDir: './',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      'react': react,
+      'react-hooks': reactHooks,
+      '@next/next': nextPlugin,
+    },
     rules: {
-      // Add any specific rules here
+      ...typescript.configs['recommended'].rules,
+      ...react.configs['recommended'].rules,
+      ...reactHooks.configs['recommended'].rules,
+      ...nextPlugin.configs['recommended'].rules,
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_' 
+      }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      'react/prop-types': 'off',
+      '@typescript-eslint/no-var-requires': 'off'
     }
   }
+];
