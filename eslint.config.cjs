870b4aa1bd3bc9acb438e7f2795af6bc42154208
Kennedy Maombi <kennedy@itskennedy.dev>
2024-10-27 02:12:08 -0500

    update

---
+++ eslint.config.cjs
@@ -0,0 +1,9 @@
+const typescript = require('@typescript-eslint/eslint-plugin');
+const typescriptParser = require('@typescript-eslint/parser');
+const react = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const nextPlugin = require('@next/eslint-plugin-next');
+
+module.exports = [
+  // ... rest of your ESLint configuration
+];
--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+// Import env.js using dynamic import
+import('./src/env.js');
 
 import path from 'path';
 import { fileURLToPath } from 'url';
