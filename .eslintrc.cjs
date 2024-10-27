9b08a319969aa5a2a02c3da40691eaace22843d2
Kennedy Maombi <kennedy@itskennedy.dev>
2024-10-27 01:34:04 -0500

    update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,7 +16,20 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended'
   ],
+  env: {
+    node: true,  // Enable Node.js global variables like 'process'
+    browser: true,  // Enables browser global variables
+    es2021: true,  // Enables modern ECMAScript globals
+  },
   rules: {
-    'react/react-in-jsx-scope': 'off' 
-  }
+    'react/react-in-jsx-scope': 'off',         // Disables the need to import React in every file with JSX
+    'react/prop-types': 'off',                 // Disables prop-types validation as you're using TypeScript
+    'no-case-declarations': 'off',             // Disables lexical declaration errors in case blocks
+    'no-undef': 'off',                         // Disables undefined variable errors (useful if you're encountering issues with global variables like 'process')
+  },
+  settings: {
+    react: {
+      version: 'detect',                       // Automatically detects the React version for linting
+    },
+  },
 }
