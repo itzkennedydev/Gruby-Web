589694850367bbc852110a8df549439f325eb4ba
Kennedy Maombi <kennedy@itskennedy.dev>
2024-10-26 21:54:59 -0500

    Fix linter errors and update configurations

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -100,4 +100,3 @@ const CardFooter = React.forwardRef<
 CardFooter.displayName = "CardFooter"
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
-export type { CardProps }
