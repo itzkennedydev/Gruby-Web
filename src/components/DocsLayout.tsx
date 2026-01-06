6d1fd9622e680d874de32724fa0330b2b64e0921
Kennedy Maombi <kennedy@itskennedy.dev>
2026-01-05 18:41:06 -0600

    Fix type import for ReactNode

--- src/components/DocsLayout.tsx
+++ src/components/DocsLayout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode } from 'react';
+import type { ReactNode } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Footer } from '@/components/Footer';
